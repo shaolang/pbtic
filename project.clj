cb156520cbe026429e5f0f224b0761ffc8df7e66
shaolang <shaolang@ymail.com>
2020-07-31 09:22:39 +0800

    Add stateful-check as dep

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
 
-  :dependencies [[org.clojure/clojure       "1.10.1"]
-                 [org.clojure/data.csv      "1.0.0"]
-                 [org.clojure/test.check    "1.0.0"]])
+  :dependencies [[org.clojars.czan/stateful-check   "0.4.2"]
+                 [org.clojure/clojure               "1.10.1"]
+                 [org.clojure/data.csv              "1.0.0"]
+                 [org.clojure/test.check            "1.0.0"]])
